af54ad590b8ac76086925d276e94383ac1d0bb29
yslaoui <yslaouiyassine@gmail.com>
2023-11-13 16:05:59 +0100

    refactored the HomeScreenCell component into its own module

---
+++ components/HomeScreenCell.js
@@ -0,0 +1,28 @@
+import {React} from 'react';
+import {  Text, View,  Image } from 'react-native';
+import { Cell } from 'react-native-tableview-simple';
+import styles from '../services/styles';
+
+const HomeScreenCell = (props) => {
+    return (
+      <Cell
+      {...props}
+      cellContentView = {
+          <View style={[styles.restaurantView, styles.container]}>
+            <Image
+              source={props.imgUri}
+              style={styles.image}
+            >
+            </Image>
+            <View style={styles.eta}>
+              <Text style={styles.etaText}>{props.eta} {'\n'} mins </Text>
+            </View>
+            <Text style={styles.restaurantHeader}>{props.title}</Text>
+            <Text style={styles.restaurantTagline}>{props.tagline}</Text>
+          </View>
+      }
+    ></Cell>
+    )
+}
+
+export default HomeScreenCell;
--- components/Restaurants.js
+++ components/Restaurants.js
@@ -2,30 +2,8 @@ import {React} from 'react';
 import {  Text, View, Button, ScrollView, SafeAreaView, Image } from 'react-native';
 import { Cell, Section, TableView } from 'react-native-tableview-simple';
 import styles from '../services/styles';
+import HomeScreenCell from './HomeScreenCell';
 
-const HomeScreenCell = (props) => {
-  return (
-    <Cell
-    {...props}
-    cellContentView = {
-        <View style={[styles.restaurantView, styles.container]}>
-          <Image
-            source={props.imgUri}
-            style={styles.image}
-          >
-          </Image>
-          <View style={styles.eta}>
-            <Text style={styles.etaText}>{props.eta} {'\n'} mins </Text>
-          </View>
-          <Text style={styles.restaurantHeader}>{props.title}</Text>
-          <Text style={styles.restaurantTagline}>{props.tagline}</Text>
-        </View>
-    }
-
-
-  ></Cell>
-  )
-}
 
 const Restaurants = ({navigation}) => 
 {
